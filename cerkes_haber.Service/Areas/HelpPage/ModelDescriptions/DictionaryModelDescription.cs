f0a7ab4a1e0ccfb7bd213f5d047cdbe1ca8c15df
Yavuz Selim Ulucan <tigris.58@hotmail.com>
2016-03-20 21:22:13 +0200

    servis değişimi

--- cerkes_haber.Service/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace cerkes_haber.Service.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- cerkes_haber.Service/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace cerkes_haber.Service.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
